9c79916a4a5bd0de08f22a042489ec0eeae08fe3
Raj Srikar <rajsrikar.d161@gmail.com>
2021-07-28 23:11:03 +0530

    [UPDATE] Added "Submit before" option
    Considering many students are having network issues, this feature was added so that they can make the tool submit the form, a few seconds prior to the deadline (instead of only 10 seconds), according to their internet connectivity

--- submitter.js
+++ submitter.js
@@ -1,18 +1,23 @@
 // Deadline calculator:
-var timeOut,hrs,mins,secs,givenHr,givenMin,timeNow,given,now,x,countDown,ok,dlChk;
+var timeOut,hrs,mins,secs,givenHr,givenMin,timeNow,given,now,x,countDown,ok,dlChk,subInps,subTime,isOk=false;
 
 function DLcheckbox() {
 	dlChk = document.getElementById('DLcheckbox');
 	if(!dlChk.checked){
 		clearTimeout(timeOut);
 		x.disabled=true;
+		subInps.disabled=true;
 		ok.disabled=true;
+		subTime.style.color='#aaa';
 		ok.style.backgroundColor='#aaa';
 	}
 	else{
 		x.disabled=false;
+		subInps.disabled=false;
 		ok.disabled=false;
+		subTime.style.color='#000';
 		ok.style.backgroundColor='#03787c';
+		isOk=false;
 	}
 }
 
@@ -25,7 +30,7 @@ function timing() {
 	mins = new Date().getMinutes();
 	secs = new Date().getSeconds();
 	timeNow =  hrs + ":" + mins;
-	given = new Date(null,null,null,givenHr,givenMin-1,50);
+	given = new Date(null,null,null,givenHr,givenMin-1,60-subInps.value);
 	now = new Date(null,null,null,hrs,mins,secs);
 	countDown = given-now;
 	if (countDown > 0) {
@@ -34,6 +39,7 @@ function timing() {
 	else{
 		ok.style.backgroundColor='#03787c';
 		alert('INVALID Deadline!');
+		isOk=false;
 		return 86400000;
 	}
 }
@@ -99,15 +105,21 @@ function clickSubmit() {
 //	console.log(hrs+":"+mins+":"+secs);
 }
 
+function setTimer() {
+	isOk=true;
+	timeOut=setTimeout(clickSubmit,timing());
+}
 
 // HTML Injection:
 function HTMLinjector() {
 	var notice = document.getElementsByClassName("office-form-notice-container");
-	var inject = '<div style="padding-top:35px;position:relative;"><div style="padding-left:20px;padding-right:20px;"><div class="question-title-box"><label style="font-weight:NORMAL;margin-left:-10px;"><input type="checkbox" id="DLcheckbox" style="bottom:0;height:18px;margin:auto 0 auto 0;position:absolute;top:0;width:20px;cursor:pointer" checked onclick="DLcheckbox()"><div class="office-form-question-title" style="margin-left:30px;margin-top:2px;cursor:pointer"><span>Deadline:</span></div></label></div><div class="office-form-question-element"><input type="time" id="deadLine"><br><br></div><button id="okButton" class="light-background-button" onclick="timeOut=setTimeout(function(){clickSubmit()},timing())">OK</button></div></div><style type="text/css">#deadLine{background-color:#fff;text-align:center;font-size:15px;height:40px;padding:12px;width:15%;}#okButton{margin:10px 0 0 20px;background:#03787c;color:#fff;font-size:15px;height:35px;text-align:center;width:50px;}@media only screen and (max-width:375px){#deadLine{width:50%;}#okButton{margin-left:0px;}}</style>';
+	var inject = '<div style="padding-top:35px;position:relative;"><div style="padding-left:20px;padding-right:20px;"><div class="question-title-box"><label style="font-weight:NORMAL;margin-left:-10px;"><input type="checkbox" id="DLcheckbox" style="bottom:0;height:18px;margin:auto 0 auto 0;position:absolute;top:0;width:20px;cursor:pointer" checked onclick="DLcheckbox()"><div class="office-form-question-title" style="margin-left:30px;margin-top:2px;cursor:pointer"><span>Deadline:</span></div></label></div><div class="office-form-question-element"><input type="time" id="deadLine" onchange="if(isOk)setTimer()"><br><label style="margin:25px 0 10px;font-weight:NORMAL;"><div style="font-size:14px;margin-top:1px;white-space: normal;margin-left:0px;display:inline;" class="office-form-question-title"><span id="subTime">Submit before:</span></div></label><select id="subInps" style="width:100px;margin-left:5px;border-radius:5px;" onchange="if(isOk)setTimer()"><option value="10">10 seconds</option><option value="20">20 seconds</option><option value="30">30 seconds</option><option value="45">45 seconds</option></select><br></div><button id="okButton" class="light-background-button" onclick="setTimer()">OK</button></div></div><style type="text/css">#deadLine{background-color:#fff;text-align:center;font-size:15px;height:40px;padding:12px;width:15%}#okButton{margin:10px 0 0 20px;background:#03787c;color:#fff;font-size:15px;height:35px;text-align:center;width:50px}@media only screen and (max-width:375px){#deadLine{width:50%}#okButton{margin-left:0px}}</style>';
 	notice[0].innerHTML = notice[0].innerHTML + inject;
 }
 
 HTMLinjector();
 
+subTime = document.getElementById('subTime');
+subInps = document.getElementById('subInps');
 ok = document.getElementById('okButton');
 x = document.getElementById('deadLine');
